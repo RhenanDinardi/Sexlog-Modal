f59cd6bba8f76afe5fec375a7cf67973a6db5cd3
rhenandinardi <rhenan.d@gmail.com>
2017-12-14 22:29:45 -0200

    branch develop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ToastrService } from 'ngx-toastr';
+
+import * as details from '../assets/json/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private toastr: ToastrService) {}
+
+  // variaveis
+  public data: any = null;
+
+  // carregamento inicial, busca dados para preenchimento da modal
+  ngOnInit() {
+
+    this.getData();
+  }
+
+  // leitura do json
+  getData() {
+
+    this.data = details;
+
+    if (!this.data.plans) {
+      console.log('no data');
+      this.toastr.error('Ocorreu um erro ao carregar as formas de pagamento.');
+    }
+    else {
+      console.log(this.data.plans);
+      this.toastr.success('Formas de pagamento carregadas com sucesso.');
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ToastrModule } from 'ngx-toastr';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      progressBar: true,
+      maxOpened: 4,
+      autoDismiss: true,
+      positionClass: 'toast-bottom-full-width'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
